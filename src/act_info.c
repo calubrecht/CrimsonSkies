ed103216eecaaf292028a45583f2e9277d4c2c8a
Blake Pell <bpell@indiana.edu>
2015-04-10 10:53:43 -0400

    Typo fix on strcmp

--- src/act_info.c
+++ src/act_info.c
@@ -937,7 +937,7 @@ void do_prompt (CHAR_DATA * ch, char *argument)
 
 	// Rhien, 04/10/2015, added advanced prompt as the start prompt, can default
 	// back to the basic one if desired.
-	if (!strcmp(argument, "all") || !strmp(argument, "reset")) {
+	if (!strcmp(argument, "all") || !strcmp(argument, "reset")) {
 		strcpy(buf, "<%hhp %mm %vmv {x({c%e{x) {g%r{c> ");
 	}
 	else if (!strcmp(argument, "basic")) {
