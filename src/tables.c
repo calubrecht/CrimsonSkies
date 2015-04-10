29cdc32c0da67ecb96fae518cbece597f2f0998e
Blake Pell <bpell@indiana.edu>
2015-04-10 17:15:58 -0400

    Added one more test clan.  I'll change the names of these later.

--- src/tables.c
+++ src/tables.c
@@ -40,8 +40,9 @@ const struct clan_type clan_table[MAX_CLAN] = {
     /*  name,       who entry,  death-transfer room,    independent */
     /* independent should be FALSE if is a real clan */
     {"", "", ROOM_VNUM_ALTAR, TRUE},
-    {"loner", "[ {WLoner{x ] ", ROOM_VNUM_ALTAR, TRUE},
-    {"malice", "[ {rMalice{x ] ", ROOM_VNUM_ALTAR, FALSE}
+    {"loner",       "[ {WLoner{x ] ",     ROOM_VNUM_ALTAR, TRUE },
+	{"knighthood",  "[ {bKnighthood{x ]", ROOM_VNUM_ALTAR, FALSE},
+    {"malice",      "[ {rMalice{x ] ",    ROOM_VNUM_ALTAR, FALSE}
 };
 
 /* for position */
