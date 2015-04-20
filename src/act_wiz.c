d026bd0036642fe6eca61ac5e5a38347e5652244
Blake Pell <bpell@indiana.edu>
2015-04-19 20:10:54 -0400

    Erwin's immortal rename function, added and updated to also work with _WIN32.

--- src/act_wiz.c
+++ src/act_wiz.c
@@ -4846,6 +4846,150 @@ void do_forcetick(CHAR_DATA * ch, char *argument)
 	return;
 }
 
+/*
+* do_rename renames a player to another name.
+* PCs only. Previous file is deleted, if it exists.
+* Char is then saved to new file.
+* New name is checked against std. checks, existing offline players and
+* online players.
+* .gz files are checked for too, just in case.
+*/
+
+bool check_parse_name(char* name);  /* comm.c */
+char *initial(const char *str);    /* comm.c */
+
+void do_rename(CHAR_DATA* ch, char* argument)
+{
+	char old_name[MAX_INPUT_LENGTH],
+		new_name[MAX_INPUT_LENGTH],
+		strsave[MAX_INPUT_LENGTH];
+
+	CHAR_DATA* victim;
+	FILE* file;
+
+	argument = one_argument(argument, old_name); /* find new/old name */
+	one_argument(argument, new_name);
+
+	/* Trivial checks */
+	if (!old_name[0])
+	{
+		send_to_char("Rename who?\n\r", ch);
+		return;
+	}
+
+	victim = get_char_world(ch, old_name);
+
+	if (!victim)
+	{
+		send_to_char("There is no such a person online.\n\r", ch);
+		return;
+	}
+
+	if (IS_NPC(victim))
+	{
+		send_to_char("You cannot use Rename on NPCs.\n\r", ch);
+		return;
+	}
+
+	/* allow rename self new_name,but otherwise only lower level */
+	if ((victim != ch) && (get_trust(victim) >= get_trust(ch)))
+	{
+		send_to_char("You failed.\n\r", ch);
+		return;
+	}
+
+	if (!victim->desc || (victim->desc->connected != CON_PLAYING))
+	{
+		send_to_char("This player has lost his link or is inside a pager or the like.\n\r", ch);
+		return;
+	}
+
+	if (!new_name[0])
+	{
+		send_to_char("Rename to what new name?\n\r", ch);
+		return;
+	}
+
+	if (!check_parse_name(new_name))
+	{
+		send_to_char("The new name is illegal.\n\r", ch);
+		return;
+	}
+
+	/* First, check if there is a player named that off-line */
+#if !defined(machintosh) && !defined(MSDOS) && !defined(_WIN32)
+	sprintf(strsave, "%s%s%s%s", PLAYER_DIR, initial(new_name),
+		"/", capitalize(new_name));
+#else
+	sprintf(strsave, "%s%s", PLAYER_DIR, capitalize(new_name));
+#endif
+
+	fclose(fpReserve); /* close the reserve file */
+	file = fopen(strsave, "r"); /* attempt to to open pfile */
+	if (file)
+	{
+		send_to_char("A player with that name already exists!\n\r", ch);
+		fclose(file);
+		fpReserve = fopen(NULL_FILE, "r"); /* is this really necessary these days? */
+		return;
+	}
+	fpReserve = fopen(NULL_FILE, "r");  /* reopen the extra file */
+
+	/* Check .gz file ! */
+#if !defined(machintosh) && !defined(MSDOS) && !defined(_WIN32)
+	sprintf(strsave, "%s%s%s%s.gz", PLAYER_DIR, initial(new_name),
+		"/", capitalize(new_name));
+#else
+	sprintf(strsave, "%s%s.gz", PLAYER_DIR, capitalize(new_name));
+#endif
+
+	fclose(fpReserve); /* close the reserve file */
+	file = fopen(strsave, "r"); /* attempt to to open pfile */
+	if (file)
+	{
+		send_to_char("A player with that name already exists in a compressed file!\n\r", ch);
+		fclose(file);
+		fpReserve = fopen(NULL_FILE, "r");
+		return;
+	}
+	fpReserve = fopen(NULL_FILE, "r");  /* reopen the extra file */
+
+	if (get_char_world(ch, new_name)) /* check for playing level-1 non-saved */
+	{
+		send_to_char("A player with the name you specified already exists!\n\r", ch);
+		return;
+	}
+
+	/* Save the filename of the old name */
+#if !defined(machintosh) && !defined(MSDOS) && !defined(_WIN32)
+	sprintf(strsave, "%s%s%s%s", PLAYER_DIR, initial(victim->name),
+		"/", capitalize(victim->name));
+#else
+	sprintf(strsave, "%s%s", PLAYER_DIR, capitalize(victim->name));
+#endif
+
+	/* Rename the character and save him to a new file */
+	/* NOTE: Players who are level 1 do NOT get saved under a new name */
+	free_string(victim->name);
+	victim->name = str_dup(capitalize(new_name));
+
+	save_char_obj(victim);
+
+	/* unlink the old file */
+#if !defined(_WIN32)
+	unlink(strsave); /* unlink does return a value.. but we do not care */
+#else
+	_unlink(strsave);
+#endif
+
+	/* That's it! */
+	send_to_char("Character renamed.\n\r", ch);
+
+	victim->position = POS_STANDING; /* I am laaazy */
+	act("$n has renamed you to $N!", ch, NULL, victim, TO_VICT);
+
+} /* do_rename */
+
 void do_debug(CHAR_DATA * ch, char *argument)
 {
 	send_to_char("Nothing here currently, move along.\r\n", ch);
--- src/interp.c
+++ src/interp.c
@@ -345,8 +345,9 @@ const struct cmd_type cmd_table[] = {
     {"prefix",		do_prefix,	POS_DEAD, IM, LOG_NORMAL, 1},
     {"mpdump",		do_mpdump,	POS_DEAD, IM, LOG_NEVER, 1},
     {"mpstat",		do_mpstat,	POS_DEAD, IM, LOG_NEVER, 1},
-    {"debug",		do_debug,   	POS_DEAD, ML, LOG_NEVER, 0},
-	{"forcetick",   do_forcetick, POS_DEAD, ML, LOG_ALWAYS, 1 },
+    {"debug",		do_debug,    POS_DEAD, ML, LOG_NEVER, 0},
+	{"forcetick",   do_forcetick,POS_DEAD, ML, LOG_ALWAYS, 1 },
+	{"rename",      do_rename,   POS_DEAD, L6, LOG_ALWAYS, 1 },
 
     /*
      * OLC
