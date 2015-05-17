bf5cee2460da4003afecbb79bf9815c3410b4205
Blake Pell <blakepell@hotmail.com>
2015-05-17 12:31:03 -0400

    Removed unused macro

--- src/db.h
+++ src/db.h
@@ -26,26 +26,22 @@
  **************************************************************************/
 
 /* vals from db.c */
-extern bool fBootDb;
+extern bool             fBootDb;
 extern int		newmobs;
 extern int		newobjs;
-extern MOB_INDEX_DATA 	* mob_index_hash          [MAX_KEY_HASH];
-extern OBJ_INDEX_DATA 	* obj_index_hash          [MAX_KEY_HASH];
+extern MOB_INDEX_DATA 	* mob_index_hash [MAX_KEY_HASH];
+extern OBJ_INDEX_DATA 	* obj_index_hash [MAX_KEY_HASH];
 extern int		top_mob_index;
 extern int		top_obj_index;
 extern int  		top_affect;
 extern int		top_ed; 
 extern AREA_DATA 	* area_first;
 
-
 /* from db2.c */
 extern int	social_count;
-
-/* macro for flag swapping */
-#define GET_UNSET(flag1,flag2)	(~(flag1)&((flag1)|(flag2)))
 
 /* Magic number for memory allocation */
 #define MAGIC_NUM 52571214
 
 /* func from db.c */
-extern void assign_area_vnum( int vnum );                    /* OLC */
+extern void assign_area_vnum( int vnum ); /* OLC */
