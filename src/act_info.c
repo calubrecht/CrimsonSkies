1f39d282a324cdda24d69cfd91a22a68ba1b2c86
Blake Pell <blakepell@hotmail.com>
2015-06-30 14:41:55 -0400

    Armor class added back to do_score

--- src/act_info.c
+++ src/act_info.c
@@ -1643,9 +1643,9 @@ void do_score(CHAR_DATA * ch, char *argument)
     printf_to_char(ch, "{gINT  : {w%2.2d{g({W%2.2d{g)    DamRoll: {w%-4d\n\r",
         get_curr_stat(ch, STAT_INT), ch->perm_stat[STAT_INT], GET_DAMROLL(ch));
 
-    printf_to_char(ch, "{gWIS  : {w%2.2d{g({W%2.2d{g)        Sex: {w%s{x\n\r",
+    printf_to_char(ch, "{gWIS  : {w%2.2d{g({W%2.2d{g)      Armor: P: {w%d{x B: {w%d{x S: {w%d{x M: {w%d{x\n\r",
         get_curr_stat(ch, STAT_WIS), ch->perm_stat[STAT_WIS],
-        ch->sex == 0 ? "Sexless" : ch->sex == 1 ? "Male" : "Female");
+        GET_AC (ch, AC_PIERCE), GET_AC (ch, AC_BASH), GET_AC (ch, AC_SLASH), GET_AC (ch, AC_EXOTIC));
 
     // Get alignment
     if (IS_GOOD(ch)) {
@@ -1661,8 +1661,10 @@ void do_score(CHAR_DATA * ch, char *argument)
     printf_to_char(ch, "{gDEX  : {w%2.2d{g({W%2.2d{g)      Align: {w%-7.7s{x           {gItems:  {w%d of %d{x\r\n",
         get_curr_stat(ch, STAT_DEX), ch->perm_stat[STAT_DEX], buf, ch->carry_number, can_carry_n(ch));
 
-    printf_to_char(ch, "{gCON  : {w%2.2d{g({W%2.2d{g)                               {gWeight: {w%d of %d{x\r\n",
-        get_curr_stat(ch, STAT_CON), ch->perm_stat[STAT_CON], ch->carry_weight, can_carry_w(ch));
+    printf_to_char(ch, "{gCON  : {w%2.2d{g({W%2.2d{g)        Sex: {w%-10.10s{x        {gWeight: {w%d of %d{x\r\n",
+        get_curr_stat(ch, STAT_CON), ch->perm_stat[STAT_CON],
+        ch->sex == 0 ? "Sexless" : ch->sex == 1 ? "Male" : "Female",
+        ch->carry_weight, can_carry_w(ch));
 
 
     printf_to_char(ch, "{gCHA  : {w%2.2d{g({W%2.2d{g)      {gWimpy: {w%-5d{x\n\r", 0, 0, ch->wimpy);
