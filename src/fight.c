31eeca4020536c075e5f6bee11769fc0f7535bd7
Blake Pell <blakepell@hotmail.com>
2015-11-05 13:53:52 -0500

    No dirt kicking underwater

--- src/fight.c
+++ src/fight.c
@@ -2580,6 +2580,9 @@ void do_dirt (CHAR_DATA * ch, char *argument)
         case (SECT_OCEAN):
             chance = 0;
             break;
+        case (SECT_UNDERWATER):
+            chance = 0;
+            break;
         case (SECT_AIR):
             chance = 0;
             break;
