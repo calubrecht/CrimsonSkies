29aaf698470c78b4ff95f4c086f1f1251cc8d298
Blake Pell <blakepell@hotmail.com>
2017-08-21 19:26:12 -0400

    Gate updated to only work on the same continent or to Limbo

--- src/magic.c
+++ src/magic.c
@@ -1997,6 +1997,7 @@ void spell_gate(int sn, int level, CHAR_DATA * ch, void *vo, int target)
         || IS_SET(ch->in_room->area->area_flags, AREA_NO_GATE)
         || victim->level >= level + 3
         || (is_clan(victim) && !is_same_clan(ch, victim))
+        || !same_continent_char(ch, victim)
         || (!IS_NPC(victim) && victim->level >= LEVEL_IMMORTAL)
         || (IS_NPC(victim) && IS_SET(victim->imm_flags, IMM_SUMMON))
         || (IS_NPC(victim) && saves_spell(level, victim, DAM_OTHER)))
